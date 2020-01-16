c148eb27de6264e82fd97c2636d1b88c9a829353
Lin Zhao <LZhao@ie.ibm.com>
2020-01-16 21:26:09 +0000

    add intro list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// 1. convert function component to class
+// 2. add state, list and date
+// 3. add date title
+// 4. add timer
+// 5. lifecycle event
+
 function App() {
   // This is a javascript array
   const list = ['Shopping', 'gym', 'Visit friend'];
