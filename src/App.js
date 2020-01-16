4b2cf80ddcb1638c6b8f042baf5301333e8bcdfe
Lin Zhao <LZhao@ie.ibm.com>
2020-01-16 16:19:21 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 function App() {
+  // This is a javascript array
+  const list = ['Shopping', 'gym', 'Visit friend'];
+
+  // This is JSX which is a mix of ui and logic
+  const listElement = (
+    <ul>
+      {list.map(todo => <li key={todo} tabindex="0">{todo}</li>)}
+    </ul>
+  );
+  
   return (
     <div>
-      
+      {listElement}
     </div>
   );
 }
