723121dd5479a0a9ff53b744401a929cbc08d42f
Lin Zhao <LZhao@ie.ibm.com>
2020-01-16 21:18:35 +0000

    add state and props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React from 'react';
 
-function App() {
-  // This is a javascript array
-  const list = ['Shopping', 'gym', 'Visit friend'];
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = { 
+      list: [
+        {text: 'Shopping', due: '2020-01-18'},
+        {text: 'gym', due: '2020-01-18'},
+        {text: 'Visit friend', due: '2020-01-18'}],
+      date: new Date(),
+    }
+  }
 
-  // This is JSX which is a mix of ui and logic
-  const listElement = (
-    <ul>
-      {list.map(todo => <li key={todo} tabindex="0">{todo}</li>)}
-    </ul>
-  );
-  
-  return (
-    <div>
-      {listElement}
-    </div>
-  );
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({date: new Date()})
+  };
+
+  render() {
+    const options = { 
+      weekday: 'long', year: 'numeric', month: 'long',
+      day: 'numeric', hour: 'numeric', 
+      minute: 'numeric', second: 'numeric' };
+
+    const today = this.state.date.toLocaleDateString('GB', options)
+
+    // This is a function component with text prop
+    const List = (props) => (<li tabIndex="0">{`${props.text}, ${props.due.toDateString()}`}</li>);
+
+    // This is JSX which is a mix of ui and logic
+    const listElement = (
+      <ul>
+        {this.state.list.map(todo => <List key={todo.text} text={todo.text} due={new Date(todo.due)} />)}
+      </ul>
+    );
+    
+    return (
+      <div>
+        <h1>{today}</h1>
+        {listElement}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/tasks.md
+++ src/tasks.md
@@ -1,7 +1,8 @@
-## 1. Syntax
-JSX
+# Syntax
+## 1. jsx
+JSXs
 - Javascript syntax extension
-- Combine ui and logic
+- Combine UI and logic
 - Specify child element, li in ul
 - Prevents Injection Attacks, everything is converted to string before rendered
 
@@ -9,3 +10,14 @@ JSX
 
 ## 2. Component & Render Element
 App.js is a React component, it is rendered into root tag on index.html
+
+# state and props
+## 1. props
+- Attribute for React Component
+- Capital letter for a component [react in depth](https://reactjs.org/docs/jsx-in-depth.html#user-defined-components-must-be-capitalized)
+- All React components must act like pure functions with respect to their props.
+
+## 2. state
+- Dynamic update value
+- function component vs class
+- lifecycle method [lifecycle method](https://reactjs.org/docs/react-component.html#mounting)
